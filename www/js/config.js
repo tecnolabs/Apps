f19eae3a3f1661504ed07891eb3420e5b68e8a88
tecnolasbs <cristiancace@gmail.com>
2015-11-22 11:19:54 -0300

    ok

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+angular.module('your_app_name.config', [])
+.constant('WORDPRESS_API_URL', 'http://app.carbonada.cl/api/')
+.constant('WORDPRESS_PUSH_URL', 'http://app.carbonada.cl/push/')
+.constant('GCM_SENDER_ID', '574597432927')
+
+;
